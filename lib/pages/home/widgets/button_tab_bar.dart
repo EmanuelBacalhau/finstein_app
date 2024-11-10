af52803e6bc066d831d1a49023e009b4a7bf3b0b
EmanuelBacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-11-10 18:09:24 -0300

    feat: add button tab bar widget

---
+++ lib/pages/home/widgets/button_tab_bar.dart
@@ -0,0 +1,39 @@
+import 'package:finstein_app/constants/app_color.dart';
+import 'package:flutter/material.dart';
+
+class ButtonTabBar extends StatelessWidget {
+  final Function() onPressed;
+  final bool isSelected;
+  final String title;
+
+  const ButtonTabBar({
+    super.key,
+    this.isSelected = false,
+    required this.title,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: TextButton.styleFrom(
+        backgroundColor: isSelected ? AppColor.lightBlue : Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4),
+          side: isSelected
+              ? BorderSide.none
+              : const BorderSide(
+                  color: AppColor.lightBlue,
+                ),
+        ),
+      ),
+      onPressed: onPressed,
+      child: Text(
+        title.substring(0, 3),
+        style: TextStyle(
+          color: isSelected ? Colors.white : AppColor.lightBlue,
+        ),
+      ),
+    );
+  }
+}
